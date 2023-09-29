cf09cec281b196c03468880f5d12799280d36776
nalinmanikandan <mnalin02@gmail.com>
2023-09-29 13:20:13 +0530

    ratings problem completed

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,9 +3,9 @@ class RatingsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @rating = @post.ratings.new(rating_params)
-
+    @topic = @post.topic
     if @rating.save
-      redirect_to topic_posts_path(@post), notice: "Rating created successfully."
+      redirect_to topic_posts_path(@topic), notice: "Rating created successfully."
     else
       render 'posts/show'
     end
